850f38cde0089e4fc1a73f9bff5b7a2437cff3d5
yinghanhung <hle.infdy@gmail.com>
2025-08-01 00:11:48 +0800

    ownership

---
+++ bank/src/main.rs
@@ -0,0 +1,63 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account { id, balance: 0, holder }
+    }
+
+    fn summary(&self) -> String {
+        format!("Account {}: {} has a balance {}", self.id, self.holder, self.balance)
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|acc| acc.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts.iter().map(|acc| acc.summary()).collect::<Vec<String>>()
+    }
+}
+
+
+fn main() {
+    let mut bank = Bank::new();
+    let mut account = Account::new(1, String::from("Wan"));
+
+    account.deposit(100);
+    account.withdraw(50);
+
+    bank.add_account(account);
+
+    println!("{:#?}", bank.summary());
+    println!("Total balance: {}", bank.total_balance());
+}
