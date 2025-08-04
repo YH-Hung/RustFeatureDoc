64a28892dbb6d315a9febff8c44f48534d9cf1e0
yinghanhung <hle.infdy@gmail.com>
2025-08-05 00:54:45 +0800

    iter intermediate impl

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -1,8 +1,24 @@
-fn print_elements(elements: &Vec<String>) {
+// vector slice is preferable than vector reference
+fn print_elements(elements: &[String]) {
     // for element in elements {
     //     println!("{}", element);
     // }
     elements.iter().for_each(|element| println!("{}", element));
+}
+
+fn shorten_strings(elements: &mut [String]) {
+    elements.iter_mut().for_each(|element| {element.truncate(1)});
+}
+
+fn to_uppercase(elements: &[String]) -> Vec<String> {
+    elements.iter()
+        .map(|element| element.to_uppercase())
+        .collect()  // collect use type annotation (returned, variable) or type argument (Turbofish) to determine the collected type
+    // collect::<Vec<String>>()
+}
+
+fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
+    vec_a.into_iter().for_each(|element| vec_b.push(element))
 }
 
 fn main() {
@@ -12,5 +28,14 @@ fn main() {
         String::from("blue"),
     ];
 
-    print_elements(&colors);
+    // print_elements(&colors);
+    // shorten_strings(&mut colors);
+    // println!("{:#?}", colors);
+
+    // let uppercase_colors = to_uppercase(&colors);
+    // println!("{:#?}", uppercase_colors);
+
+    let mut destination = vec![];
+    move_elements(colors, &mut destination);
+    println!("{:#?}", destination);
 }
